64313b6978a03655d97e2c55402f3cd0021bb563
Karol Czarnecki <kczarnecki@hotmail.com>
2012-06-24 19:39:41 +0100

    second update

--- src/Telescope.java
+++ src/Telescope.java
@@ -1,7 +1,34 @@
-import javax.swing.*; 
-import java.awt.*; 
-import java.awt.event.*;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+/**
+ * Telescope - An application to calculate the minimum and maximum magnification telescope
+ *
+ * Copyright (C) 2012 by Karol Czarnecki <kczarnecki@hotmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 public class Telescope 
 {
@@ -22,7 +49,7 @@ class SimpleFrame extends JFrame
 
 	public SimpleFrame()
 	{
-		setTitle("Telescope simple info");
+		setTitle("Telescope magnification");
 		setSize(180, 300);
 		setLocation(200,200);  
 		Panel panel = new Panel();
@@ -37,11 +64,11 @@ class Panel extends JPanel
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	JTextField SrednicaTeleskopu;
-	JTextField OgniskowaTeleskopu;
-	JTextField Jasnosc;
-	JTextField MinimalnePowiekszenie;
-	JTextField MaksymalnePowiekszenie;
+	JTextField TelescopeDiameter;
+	JTextField FocalLength;
+	JTextField Brightness;
+	JTextField MinimalMagnification;
+	JTextField MaximalMagnification;
 	//JTextField OgniskowaOkularu;
 	//JTextField UzyskanePowiekszenie;
 	
@@ -51,56 +78,58 @@ class Panel extends JPanel
 	    setLayout(new FlowLayout(FlowLayout.LEFT));
 	    setLayout(new GridLayout(12,1));  
 	    
-	    JLabel lab1 = new JLabel("Srednica teleskopu [mm]");
+	    JLabel lab1 = new JLabel("telescope diameter [mm]");
 	    add(lab1);
 	    
-	    SrednicaTeleskopu = new JTextField("",4);
-	    add(SrednicaTeleskopu);
+	    TelescopeDiameter = new JTextField("",4);
+	    add(TelescopeDiameter);
 	    
-	    JLabel lab2 = new JLabel("Ogniskowa Teleskopu [mm]");
+	    JLabel lab2 = new JLabel("focal length [mm]");
 	    add(lab2);
 	    
-	    OgniskowaTeleskopu = new JTextField("",4);
-	    add(OgniskowaTeleskopu);
+	    FocalLength = new JTextField("",4);
+	    add(FocalLength);
 	    
-	    JButton oblicz = new JButton("oblicz");
-	    add(oblicz); 
+	    JButton calculate = new JButton("calculate");
+	    add(calculate); 
 	    
-	    ActionListener sl1=new Oblicz();
-	    oblicz.addActionListener(sl1); 
+	    ActionListener sl1=new Calculate();
+	    calculate.addActionListener(sl1); 
 	    
-	    JLabel lab3 = new JLabel("jasnosc [f/  ]");   
+	    JLabel lab3 = new JLabel("brightness [f/  ]");   
 	    add(lab3);   
-	    Jasnosc = new JTextField("",4);  
-	    add(Jasnosc);
 	    
-	    JLabel lab4 = new JLabel("minimalne powiekszenie");   
+	    Brightness = new JTextField("",4);  
+	    add(Brightness);
+	    
+	    JLabel lab4 = new JLabel("minimal magnification");   
 	    add(lab4);   
-	    MinimalnePowiekszenie = new JTextField("",10);  
-	    add(MinimalnePowiekszenie);
 	    
-	    JLabel lab5 = new JLabel("maksymalne powiekszenie");   
+	    MinimalMagnification = new JTextField("",10);  
+	    add(MinimalMagnification);
+	    
+	    JLabel lab5 = new JLabel("maximal magnification");   
 	    add(lab5);   
-	    MaksymalnePowiekszenie = new JTextField("",4);  
-	    add(MaksymalnePowiekszenie);
+	    
+	    MaximalMagnification = new JTextField("",4);  
+	    add(MaximalMagnification);
 	}
 
 
-class Oblicz implements ActionListener
+class Calculate implements ActionListener
 {
 	public void actionPerformed(ActionEvent event)
 	{
 		
-		long D=Integer.parseInt(SrednicaTeleskopu.getText()); 
-		long d=Integer.parseInt(OgniskowaTeleskopu.getText());
-		long jasnosc=(long)d/D;
-		long minimalnepowiekszenie=(long)D/6;
-		long maksymalnepowiekszenie=D*2;
-		Jasnosc.setText(""+jasnosc);
-		MinimalnePowiekszenie.setText(""+minimalnepowiekszenie);
-	    MaksymalnePowiekszenie.setText(""+maksymalnepowiekszenie); 
-		
-		
+		long D=Integer.parseInt(TelescopeDiameter.getText()); 
+		long d=Integer.parseInt(FocalLength.getText());
+		long brightness=(long)d/D;
+		long minimalMagnification=(long)D/6;
+		long maximalMagnification=D*2;
+		Brightness.setText(""+brightness);
+		MinimalMagnification.setText(String.valueOf(minimalMagnification));
+		MaximalMagnification.setText(String.valueOf(maximalMagnification));
+	
 	}
 }
 
