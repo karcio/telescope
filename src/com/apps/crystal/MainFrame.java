bc0b7a6cc28b5364ded06d4ba7fd1761ab1b6156
crystal <crystal@debian>
2013-11-08 18:55:43 +0000

    redesign UI

--- src/com/apps/crystal/MainFrame.java
+++ src/com/apps/crystal/MainFrame.java
@@ -1,8 +1,15 @@
 package com.apps.crystal;
 
 import java.awt.Container;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MainFrame extends JFrame {
 	/**
@@ -11,9 +18,48 @@ public class MainFrame extends JFrame {
 	private static final long serialVersionUID = -2514433740254353298L;
 
 	public MainFrame() {
-		setTitle("Telescope magnification");
-		setSize(180, 300);
+		setIconImage(Toolkit
+				.getDefaultToolkit()
+				.getImage(
+						MainFrame.class
+								.getResource("/javax/swing/plaf/basic/icons/JavaCup16.png")));
+		setResizable(false);
+		setTitle("Telescope simple tool v0.1");
+		setSize(341, 261);
 		setLocation(200, 200);
+		setLocationRelativeTo(null);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mainMenu = new JMenu("File");
+		mainMenu.setMnemonic('F');
+		mainMenu.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		menuBar.add(mainMenu);
+
+		JMenuItem miAbout = new JMenuItem("about", 'A');
+		miAbout.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		miAbout.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("clicked: about");
+			}
+		});
+
+		JMenuItem miQuit = new JMenuItem("quit", 'Q');
+		miQuit.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		miQuit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+		mainMenu.add(miAbout);
+		mainMenu.add(miQuit);
+
 		MainPanel panel = new MainPanel();
 		Container area = getContentPane();
 		area.add(panel);
--- src/com/apps/crystal/MainPanel.java
+++ src/com/apps/crystal/MainPanel.java
@@ -1,13 +1,13 @@
 package com.apps.crystal;
 
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JTextField;
 
 public class MainPanel extends JPanel {
@@ -22,45 +22,72 @@ public class MainPanel extends JPanel {
 	JTextField maximalMagnification;
 
 	public MainPanel() {
-
-		setLayout(new FlowLayout(FlowLayout.LEFT));
-		setLayout(new GridLayout(12, 1));
+		setLayout(null);
 
 		JLabel lTelDiam = new JLabel("telescope diameter [mm]");
+		lTelDiam.setBounds(27, 10, 124, 13);
+		lTelDiam.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(lTelDiam);
 
 		telescopeDiameter = new JTextField("", 4);
+		telescopeDiameter.setBounds(156, 8, 44, 17);
+		telescopeDiameter.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(telescopeDiameter);
 
 		JLabel lFocalLength = new JLabel("focal length [mm]");
+		lFocalLength.setBounds(27, 37, 87, 13);
+		lFocalLength.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(lFocalLength);
 
 		focalLength = new JTextField("", 4);
+		focalLength.setBounds(156, 37, 44, 17);
+		focalLength.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(focalLength);
 
 		JButton bCalculate = new JButton("calculate");
+		bCalculate.setBounds(123, 67, 76, 23);
+		bCalculate.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(bCalculate);
 
-		ActionListener aListener = new Calculate();
-		bCalculate.addActionListener(aListener);
-
 		JLabel lBrightness = new JLabel("brightness [f/  ]");
+		lBrightness.setBounds(27, 123, 74, 13);
+		lBrightness.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(lBrightness);
 
 		brightness = new JTextField("", 4);
+		brightness.setEditable(false);
+		brightness.setBounds(156, 121, 44, 17);
+		brightness.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(brightness);
 
 		JLabel lMinMagn = new JLabel("minimal magnification");
+		lMinMagn.setBounds(27, 152, 106, 13);
+		lMinMagn.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(lMinMagn);
 
 		minimalMagnification = new JTextField("", 10);
+		minimalMagnification.setEditable(false);
+		minimalMagnification.setBounds(156, 150, 44, 17);
+		minimalMagnification.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(minimalMagnification);
 
 		JLabel lMaxMagn = new JLabel("maximal magnification");
+		lMaxMagn.setBounds(27, 181, 110, 13);
+		lMaxMagn.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(lMaxMagn);
 
 		maximalMagnification = new JTextField("", 4);
+		maximalMagnification.setEditable(false);
+		maximalMagnification.setBounds(156, 179, 44, 17);
+		maximalMagnification.setFont(new Font("Segoe UI", Font.PLAIN, 10));
 		add(maximalMagnification);
+
+		JSeparator separator = new JSeparator();
+		separator.setBounds(27, 102, 284, 2);
+		add(separator);
+
+		ActionListener aListener = new Calculate();
+		bCalculate.addActionListener(aListener);
 	}
 
 	class Calculate implements ActionListener {
