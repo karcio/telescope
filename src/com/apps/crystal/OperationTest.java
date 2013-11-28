a61760e31c527af76eb1cf2b6bcfde0b0ef5dadf
crystal <crystal@localhost>
2013-11-28 18:05:56 +0000

    add more Junits

--- src/com/apps/crystal/OperationTest.java
+++ src/com/apps/crystal/OperationTest.java
@@ -6,12 +6,13 @@ import org.junit.Test;
 
 public class OperationTest {
 
-
 	@Test
-	public void testDivide() {
+	public void testCalculate() {
 		Operation oper = new Operation();
 		oper.calculate(650, 130);
 		Assert.assertEquals(5, oper.getBrightness());
+		Assert.assertEquals(21, oper.getMinimalMagnification());
+		Assert.assertEquals(260, oper.getMaximalMagnification());
 
 	}
 
